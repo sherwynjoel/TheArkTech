b9c5c598e6c1ba8d20667c17d77ed23f1020fece
yashistrash <justyash69@gmail.com>
2025-08-28 21:56:23 +0530

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   server: { host: true, port: 4321 },
   output: 'static',
-  integrations: [tailwind()],
+  site: 'https://thearktech.in',
+  integrations: [tailwind(), sitemap()],
 });
 
